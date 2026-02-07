81c9c62d9608109f9f1cd2475f141ce1f0ca766b
Gustavo Reyes <gustavoreyes@Gustavos-MacBook-Pro.local>
2026-02-07 18:17:08 -0300

    se corrige redis port

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ OPENAI_MODEL = os.getenv("OPENAI_MODEL", "gpt-4o")
 EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL", "text-embedding-ada-002")
 
 REDIS_HOST = os.getenv("REDIS_HOST", "")
-REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
+REDIS_PORT = int(os.getenv("REDIS_PORT", "12419"))
 REDIS_INDEX = os.getenv("REDIS_INDEX", "grupo_13")
 REDIS_USERNAME = os.getenv("REDIS_USERNAME", "default")
 REDIS_DB = int(os.getenv("REDIS_DB", "0"))
